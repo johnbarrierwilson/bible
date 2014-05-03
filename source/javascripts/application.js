8df8046262fb3a7788bebb91d31a302e26aa7e22
John Wilson <john@j-wilson.com>
2014-05-03 13:32:48 -0400

    Minor performance and visual improvements such as highlight color/animation

---
+++ source/javascripts/application.js
@@ -0,0 +1,5 @@
+var editor = new MediumEditor('.reader', {
+  // buttons: ['bold', 'italic', 'underline', 'anchor', 'header1', 'header2', 'quote', 'superscript', 'subscript', 'strikethrough', 'unorderedlist', 'orderedlist', 'pre', 'image', 'indent', 'outdent'],
+  buttons: ['bold', 'italic', 'quote'],
+  disableEditing: true
+});
